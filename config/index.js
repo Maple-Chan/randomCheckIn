a7c96ad65c63a009c76dfa4337c68201123b212f
Maple Chan <f310815007@outlook.com>
2024-11-09 13:17:37 +0800

    [deploy] upload 1.0.4

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: 'https://blog.maplestory.work/page/app/randomCheckIn/',
 
     /**
      * Source Maps
